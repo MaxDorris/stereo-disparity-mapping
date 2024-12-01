7c18e76e60dc9c67e490bfc7d9a522a24355fd0a
max <max@nomad-2.local>
2024-12-01 13:11:19 -0600

    initial code for depthmap algo

---
+++ depthMap.py
@@ -0,0 +1,45 @@
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+import os
+
+class DepthMap:
+    def __init__(self,showImages):
+
+        # Load images
+        root = os.getcwd()
+        imgLeftPath = os.path.join(root, 'demoImages//motorcycle//im0.png')
+        imgRightPath = os.path.join(root, 'demoImages//motorcycle//im1.png')
+        self.imgLeft = cv.imread(imgLeftPath,cv.IMREAD_GRAYSCALE)
+        self.imgRight = cv.imread(imgRightPath,cv.IMREAD_GRAYSCALE)
+
+        if showImages: # plot the images next to eachother
+            plt.figure()
+            plt.subplot(121)
+            plt.imshow(self.imgLeft)
+            plt.subplot(122)
+            plt.imshow(self.imgRight)
+            plt.show()
+
+    def computeDepthMapBM(self):
+        nDispFactor = 6 # adjustable
+        stereo = cv. StereoBM.create(numDisparities=16*nDispFactor,
+                                     blockSize=21)
+        disparity = stereo.compute(self.imgLeft, self.imgRight)
+        plt.imshow(disparity,'gray')
+        plt.show()
+    
+    def computeDepthMapSGBM(self):
+        window_size = 7
+        min_disp = 16
+        nDispFactor = 14 # adjustable (14 is good)
+
+def demoViewPics():
+    # initializes an object of class "DepthMap", passing in the extra arg of showImages=True so that the images are plotted after being loaded in after initialization
+    dp = DepthMap(showImages=True)
+
+
+if __name__ == '__main__':
+    demoViewPics()
+    # demoStereoBM()
+    # demoStereoSGBM()
