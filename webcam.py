78c94815064d1f9c5e34d843f67d6d9f0ed3b537
MaxDorris <dorrismt@yayasci.com>
2025-01-03 11:34:10 -0600

    adding requirements

--- webcam.py
+++ webcam.py
@@ -1,31 +1,55 @@
 import cv2
+import time
 from cv2_enumerate_cameras import enumerate_cameras
 
-for camera_info in enumerate_cameras():
-    print(f'{camera_info.index}: {camera_info.name}')
+def main():
 
+    for camera_info in enumerate_cameras():
+        print(f'{camera_info}')
 
+    # Access the webcam (usually 0 for the default camera)
+    cam_list = []
+    for i in range(15):
+        print(f'Connecting to Camera {i}')
+        try:
+            cap = cv2.VideoCapture(i, cv2.CAP_DSHOW)
+        except:
+            print('NO CAMERA FOUND FOR {i}')
+            reset_camera(cap)
+            continue
 
-# # Access the webcam (usually 0 for the default camera)
-# cap = cv2.VideoCapture(0)
+        print(f'Testing camera {i}\n')
+        timer = time.time()
+        while True:
+            tick = time.time()
+            # Read a frame from the webcam
+            ret, frame = cap.read()
 
+            # Check if the frame was captured successfully
+            if not ret:
+                break
 
+            # Display the frame
+            cv2.imshow(f"Webcam Stream {i}", frame)
 
-# while True:
-#     # Read a frame from the webcam
-#     ret, frame = cap.read()
+            # Exit the loop if the 'q' key is pressed
+            if tick - timer >= 8:
+                print(f'Camera {i}: NOT FOUND')
+                reset_camera(cap)
+                break
+            elif cv2.waitKey(1) == ord('q'):
+                print(f'Camera {i}: FOUND')
+                cam_list.append(i)
+                print(f"Camera {i} added to cam_list.")
+                reset_camera(cap)
+                break
 
-#     # Check if the frame was captured successfully
-#     if not ret:
-#         break
+def reset_camera(cap):
+    # Release the webcam and close the window
+    print('Disconnecting from camera and resetting window\n')
+    cap.release()
+    cv2.destroyAllWindows()
+    time.sleep(5)
 
-#     # Display the frame
-#     cv2.imshow("Webcam Stream", frame)
-
-#     # Exit the loop if the 'q' key is pressed
-#     if cv2.waitKey(1) == ord('q'):
-#         break
-
-# # Release the webcam and close the window
-# cap.release()
-# cv2.destroyAllWindows()
+if __name__=='__main__':
+    main()
